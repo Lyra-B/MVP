abee51a4d72260e4610dae451f15b16c9f46393f
Lyra-B <glykeriapeppa@gmail.com>
2015-04-15 22:56:06 +0100

    Removed resources from sessions(generated by the gem now)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   mount FullcalendarEngine::Engine => "/calendar"
+
+  # resources :sessions, :only => [:create]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
