6c8dc215eb224d2ab3f486f11916ad2a9151cdb6
Lyra-B <glykeriapeppa@gmail.com>
2015-04-18 23:19:46 +0100

    Refactored factory girl for users

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,19 +1,18 @@
 FactoryGirl.define do
-  factory :user do
-    factory :coach do
-      firstname "James"
-      lastname "Brown"
-      email "james@mail.com"
-      password "password"
-      type "Coach"
-    end
-
-    factory :administrator do
-      firstname "Derek"
-      lastname "Brown"
-      email "derek@mail.com"
-      password "password"
-      type "Administrator"
-    end
+  factory :coach do
+    firstname "James"
+    lastname "Brown"
+    email "james@mail.com"
+    password "password"
+    type "Coach"
+  end
+end
+FactoryGirl.define do
+  factory :administrator do
+    firstname "Derek"
+    lastname "Brown"
+    email "derek@mail.com"
+    password "password"
+    type "Administrator"
   end
 end
