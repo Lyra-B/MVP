a0cb1d19b0f445f29ae8cf655cdb604a240d91d0
Lyra-B <glykeriapeppa@gmail.com>
2015-04-19 18:52:07 +0100

    Drag and drop coach onto session and assigning coach_id

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -3,8 +3,50 @@ $(document).ready(function(){
     FullcalendarEngine.Form.display();
     event.preventDefault();
   });
+  //set additional options for sessions
+  var options = {
+    sessionRender: function(session, element) {
+      element.data("session_id", session.id);
+    }
+  };
+  //include these options to the gem's settings
+  $.extend(full_calendar_options, options);
   $('.calendar').fullCalendar(full_calendar_options);
+
+  // wrap ul element
+  var coaches = $("#drop1");
+
+  //make every coach draggable
+  $("li", coaches).draggable({
+    revert: "invalid", // the object goes back to default position if not dropped
+    helper: "clone", // duplicates the element to use in both places
+    start: function(event, ui){
+      var sessions = $(".fc-session"); // sessions
+      var coachId = $(this).data("coach-id");
+      console.log("hello");
+      sessions.droppable({
+        accept: "#drop1 > li",
+        activeClass: "ui-state-highlight",
+        drop: function(event, ui) {
+          // console.log("");
+          var sessionId = $(this).data("session_id");
+          $.ajax({
+            method:"PUT",
+            url: "/calendar/sessions/" + sessionId + "/assign_coach/" + coachId,
+            success: function(){
+              alert("Dropped");
+            }
+          });
+        }
+      });
+    }
+
+
+  });
+
 });
+
+
 
 
 
