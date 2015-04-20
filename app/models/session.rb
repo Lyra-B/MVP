dff071ec494904fac893f080c5b9cf9663479c52
Lyra-B <glykeriapeppa@gmail.com>
2015-04-20 20:51:15 +0100

    Added color and assigned? method

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,3 +1,16 @@
 class Session < FullcalendarEngine::Session
   belongs_to :user
+
+  def assigned?
+    true unless self.coach_id == nil
+  end
+
+  def color_state
+    if self.assigned?
+      self.color = "#4DFA90"
+    else
+      self.color = "#FF5468"
+    end
+    self.color
+  end
 end
