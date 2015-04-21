0f47900e479a1b299f6ad863709bdc26357bcda1
Lyra-B <glykeriapeppa@gmail.com>
2015-04-21 13:45:04 +0100

    Created assignments

---
+++ spec/controllers/assignments_controller_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe AssignmentsController, type: :controller do
+#   context "Administrator" do
+#     login_administrator
+
+#     before do
+#       @administrator = Administrator.first
+#       @coach = FactoryGirl.create(:coach)
+#       @session = FactoryGirl.create(:session)
+#     end
+
+#     describe "Post to create" do
+#       before do
+#         params = {
+#           assignment: {
+#             session_id: @session.id,
+#             coach_id: @coach.id
+#           }
+#         }
+#         post :create, params
+#       end
+
+#       it("should save the assignment") do
+#         expect(Assignment.count).to eq(1)
+#       end
+
+#       it("should have an administrator id") do
+#         expect(Assignment.last.administrator_id).to eq(@administrator.id)
+#       end
+
+#       it("should have a waiting status") do
+#         expect(Assignment.last.status).to eq("waiting")
+#       end
+#     end
+#   end
+end
---
+++ spec/factories/assignments.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :assignment do
+    coach_id 1
+administrator_id 1
+session_id 1
+status "MyString"
+  end
+
+end
