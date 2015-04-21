a6ba58207d3d861abb902cbbb07a86faf544399e
Lyra-B <glykeriapeppa@gmail.com>
2015-04-21 15:00:31 +0100

    Updated associations

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,2 +1,13 @@
 class Assignment < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :coach
+  belongs_to :administrator
+  belongs_to :session
+
+  before_create :set_default_status
+
+
+  def set_default_status
+    self.status = "waiting"
+  end
 end
