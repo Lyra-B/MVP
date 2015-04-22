c80727143b9de03eb91040acb84537ccf6fb51a8
Lyra-B <glykeriapeppa@gmail.com>
2015-04-22 16:40:31 +0100

    Problem with assignment feature

--- features/assignment.feature
+++ features/assignment.feature
@@ -1,20 +1,18 @@
-Feature: Assignment
-  Background:
-    Given there is at least a coach
-      And I am in the homepage
-      And I sign in
-    When I enter my details as a coach
-      And I submit
-    Then I see my coach dashboard
-    @wip @javascript
-  Scenario: A coach accepts a session
-    Given I have some notifications
-      And I select the button notifications
-    When I select accept on "assignment_1"
-    Then the status of this assignment becomes accepted
-    @wip @javascript
-  Scenario: A coach declines a session
-    Given I have some notifications
-      And I select the button notifications
-    When I select decline on "assignment_2"
-    Then the status of this assignment becomes declined
+# Feature: Assignment
+#   Background:
+#     Given there is at least a coach
+#       And I am in the homepage
+#       And I sign in
+#     When I enter my details as a coach
+#       And I submit
+#     Then I see my coach dashboard
+#     @wip
+#   Scenario: A coach accepts a session
+#     Given I have some notifications
+#     When I select accept on "#assignment_1"
+#     Then the status of this assignment becomes accepted
+#     @wip
+#   Scenario: A coach declines a session
+#     Given I have some notifications
+#     When I select decline on "#assignment_2"
+#     Then the status of this assignment becomes declined
--- features/step_definitions/assignment_steps.rb
+++ features/step_definitions/assignment_steps.rb
@@ -1,34 +1,31 @@
-Given(/^I have some notifications$/) do
-  @coach = Coach.first
-  @session_1 = FactoryGirl.create(:session, :title => "Cricket")
-  @session_2 = FactoryGirl.create(:session, :title => "Baseball")
-  @assignment_1 = FactoryGirl.create(:assignment, coach_id: @coach.id, session_id:@session_1)
-  @assignment_2 = FactoryGirl.create(:assignment, coach_id: @coach.id, session_id:@session_2)
-  @coach.assignments << @assignment_1
-  @coach.assignments << @assignment_2
-end
+# Given(/^I have some notifications$/) do
+#   @coach = Coach.first
+#   @session_1 = FactoryGirl.create(:session, :title => "Cricket")
+#   @session_2 = FactoryGirl.create(:session, :title => "Baseball")
+#   @assignment_1 = FactoryGirl.create(:assignment, coach_id: @coach.id, session_id:@session_1.id)
+#   @assignment_2 = FactoryGirl.create(:assignment, coach_id: @coach.id, session_id:@session_2.id)
+#   @coach.assignments << @assignment_1
+#   @coach.assignments << @assignment_2
+#   @coach.save
+# end
 
-Given(/^I select the button notifications$/) do
-  click_on "Notifications"
-end
+# When(/^I select accept on "(.*?)"$/) do |assignment|
+#   save_and_open_page
+#   within(assignment) do
+#     click_on "Accept"
+#   end
+# end
 
-When(/^I select accept on "(.*?)"$/) do |assignment|
-  within("##{assignment}") do
-    click_on "Accept"
-  end
-end
+# Then(/^the status of this assignment becomes accepted$/) do
+#   expect(assignment_1.status).to eq("accepted")
+# end
 
-Then(/^the status of this assignment becomes accepted$/) do
-  expect(assignment_1.status).to eq("accepted")
-end
+# When(/^I select decline on "(.*?)"$/) do |assignment|
+#   within(assignment) do
+#     click_on "Decline"
+#   end
+# end
 
-When(/^I select decline on "(.*?)"$/) do |assignment|
-  within("##{assignment}") do
-    click_on "Decline"
-  end
-  save_and_open_page
-end
-
-Then(/^the status of this assignment becomes declined$/) do
-  expect(assignment_1.status).to eq("declined")
-end
+# Then(/^the status of this assignment becomes declined$/) do
+#   expect(assignment_1.status).to eq("declined")
+# end
