941bddc8df87eeecafc5fd6afa7e6054be989b95
Lyra-B <glykeriapeppa@gmail.com>
2015-04-22 16:47:50 +0100

    Filtered assignments

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,6 @@
 class DashboardsController < ApplicationController
   def show
     @coaches = Coach.all
-    @assignments = current_user.assignments
+    @assignments = current_user.assignments.where(:status => "waiting")
   end
 end
