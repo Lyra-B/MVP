782d693dd71812c3151cb86d1eeacc2358639c20
Lyra-B <glykeriapeppa@gmail.com>
2015-04-23 14:52:16 +0100

    Added simplecov

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,12 @@
+require 'simplecov'
+SimpleCov.start 'rails'
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+
 require 'devise'
 require 'support/controller_macros'
 
